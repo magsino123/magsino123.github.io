4a5e181df8e89682d7e300bb63a6f2e6d4783ec2
magsino123 <36427923+magsino123@users.noreply.github.com>
2020-10-09 16:51:15 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/my.portfolio/';
+$config['base_url'] = 'https://magsino123.github.io/my.portfolio.github.io/';
 
 /*
 |--------------------------------------------------------------------------
